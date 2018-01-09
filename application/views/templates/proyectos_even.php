a494f40f4de5f495730d211ff99c9008f91e0e87
DANIEL BENSON <bensondaniel664@gmail.com>
2018-01-09 12:18:35 -0600

    fixed proyectos links conoce mas

--- application/views/templates/proyectos_even.php
+++ application/views/templates/proyectos_even.php
@@ -34,7 +34,7 @@
                                                 <div class="screen mx-auto text-center" style="width: 100%">
                                                 <p class="caption"><?php echo $posts[$b]['titulo'];?></p>
                                                 <p class="small">| CÓDIGO UB |</p>
-                                                <a class="btn btn-outline-secondary leer-mas" href="#">Conoce más</a>
+                                                <a class="btn btn-outline-secondary leer-mas" href="<?php echo base_url('proyectos/' . $posts[$b]['slug']);?>">Conoce más</a>
                                             </div>
                                                     <img class="item img-proyectos img-fluid" src="<?php echo base_url('uploads/' . $image_name[0]['nombre']);?>" alt="">
                                                 </div>
@@ -52,7 +52,7 @@
                                         <div class="screen mx-auto text-center" style="width: 100%">
                                             <p class="caption"><?php echo $posts[$i]['titulo'];?></p>
                                             <p class="small">| CÓDIGO UB |</p>
-                                            <a class="btn btn-outline-secondary leer-mas" href="#">Conoce más</a>
+                                            <a class="btn btn-outline-secondary leer-mas" href="<?php echo base_url('proyectos/' . $posts[$i]['slug']);?>">Conoce más</a>
                                         </div>
                                     <img class="item img-proyectos img-fluid w-100" src="<?php echo base_url('uploads/' . $image_name[0]['nombre']);?>" alt="">
                                     </div>
@@ -64,7 +64,7 @@
                                 <div class="screen mx-auto text-center" style="width: 100%">
                                     <p class="caption"><?php echo $posts[$i]['titulo'];?></p>
                                     <p class="small">| CÓDIGO UB |</p>
-                                    <a class="btn btn-outline-secondary leer-mas" href="#">Conoce más</a>
+                                    <a class="btn btn-outline-secondary leer-mas" href="<?php echo base_url('proyectos/' . $posts[$i]['slug']);?>">Conoce más</a>
                                 </div>
                             <img class="item img-proyectos item img-fluid w-100" src="<?php echo base_url('uploads/' . $image_name[0]['nombre']);?>" alt="">
                             </div>
@@ -88,7 +88,7 @@
                                             <div class="screen mx-auto text-center" style="width: 100%">
                                                 <p class="caption"><?php echo $posts[$b]['titulo'];?></p>
                                                 <p class="small">| CÓDIGO UB |</p>
-                                                <a class="btn btn-outline-secondary leer-mas" href="#">Conoce más</a>
+                                                <a class="btn btn-outline-secondary leer-mas" href="<?php echo base_url('proyectos/' . $posts[$b]['slug']);?>">Conoce más</a>
                                             </div>
                                             <img class="item img-proyectos img-fluid" src="<?php echo base_url('uploads/' . $image_name[0]['nombre']);?>" alt="">
                                         </div>
